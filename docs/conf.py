ebe37a31f82a0d1ab178ab63f493abbf61134142
esohkevin <esohkevin4@gmail.com>
2026-09-10 18:28:58 -0400

    updates

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,9 @@ extensions = [
 ]
 
 html_static_path = ['_static']
+#Specify the filename of your custom logo
+html_logo = '_static/pycmplot-logo-circular.png' 
+
 html_css_files = ["tippy.css"]
 tippy_add_class = "has-tippy"
 
